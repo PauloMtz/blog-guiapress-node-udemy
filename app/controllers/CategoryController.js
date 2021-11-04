5c32ad73b6b80fa21a7cfaeaa9af159f99205764
PramosMtz <pramosmtz@gmail.com>
2021-11-04 20:15:57 -0300

    Crud categoria - cadastro de categoria

--- app/controllers/CategoryController.js
+++ app/controllers/CategoryController.js
@@ -1,1 +1,32 @@
 const categoryModel = require("../models/Category");
+// slugify é uma biblioteca do node para trabalhar com slug
+// instalação [ npm install slugify --save ]
+const slugify = require("slugify");
+
+module.exports = function (application) {
+  application.get("/admin/categories", function (req, res) {
+    res.render("admin/categories/new");
+  });
+
+  application.post("/admin/categories/save", function (req, res) {
+    var title = req.body.title;
+
+    if (title != undefined) {
+      // verificar antes o que está sendo enviado
+      // res.send("Título: " + title);
+
+      // se estiver ok -> salva no banco de dados
+      categoryModel
+        .create({
+          // titulo no banco recebe a variável titulo
+          title: title,
+          slug: slugify(title),
+        })
+        .then(() => {
+          res.redirect("/");
+        });
+    } else {
+      res.redirect("admin/categories/new");
+    }
+  });
+};
