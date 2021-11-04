3cd927afbd05a3ab0ce3250a29d43de2e8a0c5c4
PramosMtz <pramosmtz@gmail.com>
2021-11-04 20:54:36 -0300

    Crud categorias - listando categorias

--- app/controllers/CategoryController.js
+++ app/controllers/CategoryController.js
@@ -4,10 +4,19 @@ const categoryModel = require("../models/Category");
 const slugify = require("slugify");
 
 module.exports = function (application) {
+  // carrega a página de lista
   application.get("/admin/categories", function (req, res) {
+    categoryModel.findAll().then((categories) => {
+      res.render("admin/categories/index", { categories: categories });
+    });
+  });
+
+  // carrega a página de cadastro
+  application.get("/admin/categories/new", function (req, res) {
     res.render("admin/categories/new");
   });
 
+  // cadastra categoria
   application.post("/admin/categories/save", function (req, res) {
     var title = req.body.title;
 
@@ -23,7 +32,7 @@ module.exports = function (application) {
           slug: slugify(title),
         })
         .then(() => {
-          res.redirect("/");
+          res.redirect("/admin/categories");
         });
     } else {
       res.redirect("admin/categories/new");
